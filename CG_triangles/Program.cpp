c053436da06c2ad8f85ae597ded69d987f46a3e5
Tomasz Próchniewicz <tommpoland@gmail.com>
2016-11-13 21:06:57 +0100

    new version

--- CG_triangles/Program.cpp
+++ CG_triangles/Program.cpp
@@ -22,23 +22,26 @@ void paintTraingle(float x_1, float y_1, float x_2, float y_2, Color *color)
 {
 	glColor3f(color->Red(), color->Green(), color->Blue());
 	glBegin(GL_TRIANGLES);
-	glVertex3f(x_1, y_1, 0.0f);
-	glVertex3f(x_2, y_1, 0.0f);
-	glVertex3f(x_1, y_2, 0.0f);
+		glVertex3f(x_1, y_1, 0.0f);
+		glVertex3f(x_2, y_1, 0.0f);
+		glVertex3f(x_1, y_2, 0.0f);
 	glEnd();
 }
 
-void rotateTraingle(float displacement_x, float displacement_y)
+void rotateTraingle(float angle, float displacement_x, float displacement_y, float around_x, float around_y)
 {
-	glTranslatef(-displacement_x, -displacement_y, 0);
+	//glTranslatef(displacement_x, displacement_y, 0);
+	glTranslatef(displacement_x + around_x, displacement_y + around_y, 0);
 	glRotatef(angle, 0.0f, 0.0f, 1.0f);
-	glTranslatef(displacement_x, displacement_y, 0);
+	glTranslatef(-around_x - displacement_x, -around_y - displacement_y, 0);
 }
 
-void drawAndAnimateTriangle(float displacement_x, float displacement_y, float x_1, float y_1, float x_2, float y_2, Color *color)
+void drawAndAnimateTriangle(float angle, float displacement_x, float displacement_y, float x_1, float y_1, float x_2, float y_2, Color *color)
 {
+	float x_avg = (2 * x_1 + x_2) / 3;
+	float y_avg = (2 * y_1 + y_2) / 3;
 	glPushMatrix();
-	rotateTraingle(displacement_x, displacement_y);
+	rotateTraingle(angle, displacement_x, displacement_y, x_avg, y_avg);
 	paintTraingle(x_1, y_1, x_2, y_2, color);
 	glPopMatrix();
 }
@@ -53,60 +56,69 @@ void renderScene(void)
 			  0.0f, 1.0f, 0.0f);
 
 	glPushMatrix();
-		glRotatef(-3 * angle, 0.0f, 0.0f, 1.0f);
+	glRotatef(3 * angleCenter, 0.0f, 0.0f, 1.0f);
 		// 4 inner blue
-		drawAndAnimateTriangle(displacement, displacement, 0.0f, 0.0f, 1.0f, 1.0f, colorBlue);
-		drawAndAnimateTriangle(-displacement, displacement, 0.0f, 0.0f, -1.0f, 1.0f, colorBlue);
-		drawAndAnimateTriangle(displacement, -displacement, 0.0f, 0.0f, 1.0f, -1.0f, colorBlue);
-		drawAndAnimateTriangle(-displacement, -displacement, 0.0f, 0.0f, -1.0f, -1.0f, colorBlue);
+		drawAndAnimateTriangle(-2 * angleObject, displacement, displacement, 0.0f, 0.0f, 1.0f, 1.0f, colorBlue);
+		drawAndAnimateTriangle(-2 * angleObject, -displacement, displacement, 0.0f, 0.0f, -1.0f, 1.0f, colorBlue);
+		drawAndAnimateTriangle(-2 * angleObject, displacement, -displacement, 0.0f, 0.0f, 1.0f, -1.0f, colorBlue);
+		drawAndAnimateTriangle(-2 * angleObject, -displacement, -displacement, 0.0f, 0.0f, -1.0f, -1.0f, colorBlue);
 	glPopMatrix();
 
 	glPushMatrix();
-		glRotatef(angle, 0.0f, 0.0f, 1.0f);
+		glRotatef(-angleCenter, 0.0f, 0.0f, 1.0f);
 		// 4 midlle orange
-		drawAndAnimateTriangle(2 * displacement, displacement, 1.0f, 0.0f, 2.0f, 1.0f, colorOrange);
-		drawAndAnimateTriangle(-displacement, 2 * displacement, 0.0f, 1.0f, -1.0f, 2.0f, colorOrange);
-		drawAndAnimateTriangle(-2 * displacement, -displacement, -1.0f, 0.0f, -2.0f, -1.0f, colorOrange);
-		drawAndAnimateTriangle(displacement, -2 * displacement, 0.0f, -1.0f, 1.0f, -2.0f, colorOrange);
+		drawAndAnimateTriangle(angleObject, 2 * displacement, displacement, 1.0f, 0.0f, 2.0f, 1.0f, colorOrange);
+		drawAndAnimateTriangle(angleObject, -displacement, 2 * displacement, 0.0f, 1.0f, -1.0f, 2.0f, colorOrange);
+		drawAndAnimateTriangle(angleObject, -2 * displacement, -displacement, -1.0f, 0.0f, -2.0f, -1.0f, colorOrange);
+		drawAndAnimateTriangle(angleObject, displacement, -2 * displacement, 0.0f, -1.0f, 1.0f, -2.0f, colorOrange);
 
 		// 4 middle green
-		drawAndAnimateTriangle(displacement, 2 * displacement, 0.0f, 1.0f, 1.0f, 2.0f, colorGreen);
-		drawAndAnimateTriangle(-2 * displacement, displacement, -1.0f, 0.0f, -2.0f, 1.0f, colorGreen);
-		drawAndAnimateTriangle(-displacement, -2 * displacement, 0.0f, -1.0f, -1.0f, -2.0f, colorGreen);
-		drawAndAnimateTriangle(2 * displacement, -displacement, 1.0f, 0.0f, 2.0f, -1.0f, colorGreen);
+		drawAndAnimateTriangle(angleObject, displacement, 2 * displacement, 0.0f, 1.0f, 1.0f, 2.0f, colorGreen);
+		drawAndAnimateTriangle(angleObject, -2 * displacement, displacement, -1.0f, 0.0f, -2.0f, 1.0f, colorGreen);
+		drawAndAnimateTriangle(angleObject, -displacement, -2 * displacement, 0.0f, -1.0f, -1.0f, -2.0f, colorGreen);
+		drawAndAnimateTriangle(angleObject, 2 * displacement, -displacement, 1.0f, 0.0f, 2.0f, -1.0f, colorGreen);
 
 		// 4 outer red
-		drawAndAnimateTriangle(displacement, 3 * displacement, 0.0f, 2.0f, 1.0f, 3.0f, colorRed);
-		drawAndAnimateTriangle(3 * displacement, -displacement, 2.0f, 0.0f, 3.0f, -1.0f, colorRed);
-		drawAndAnimateTriangle(-displacement, -3 * displacement, 0.0f, -2.0f, -1.0f, -3.0f, colorRed);
-		drawAndAnimateTriangle(-3 * displacement, displacement, -2.0f, 0.0f, -3.0f, 1.0f, colorRed);
+		drawAndAnimateTriangle(angleObject, displacement, 3 * displacement, 0.0f, 2.0f, 1.0f, 3.0f, colorRed);
+		drawAndAnimateTriangle(angleObject, 3 * displacement, -displacement, 2.0f, 0.0f, 3.0f, -1.0f, colorRed);
+		drawAndAnimateTriangle(angleObject, -displacement, -3 * displacement, 0.0f, -2.0f, -1.0f, -3.0f, colorRed);
+		drawAndAnimateTriangle(angleObject, -3 * displacement, displacement, -2.0f, 0.0f, -3.0f, 1.0f, colorRed);
 
 		// 4 outer yellow
-		drawAndAnimateTriangle(2 * displacement, 2 * displacement, 1.0f, 1.0f, 2.0f, 2.0f, colorYellow);
-		drawAndAnimateTriangle(2 * displacement, -2 * displacement, 1.0f, -1.0f, 2.0f, -2.0f, colorYellow);
-		drawAndAnimateTriangle(-2 * displacement, -2 * displacement, -1.0f, -1.0f, -2.0f, -2.0f, colorYellow);
-		drawAndAnimateTriangle(-2 * displacement, 2 * displacement, -1.0f, 1.0f, -2.0f, 2.0f, colorYellow);
+		drawAndAnimateTriangle(angleObject, 2 * displacement, 2 * displacement, 1.0f, 1.0f, 2.0f, 2.0f, colorYellow);
+		drawAndAnimateTriangle(angleObject, 2 * displacement, -2 * displacement, 1.0f, -1.0f, 2.0f, -2.0f, colorYellow);
+		drawAndAnimateTriangle(angleObject, -2 * displacement, -2 * displacement, -1.0f, -1.0f, -2.0f, -2.0f, colorYellow);
+		drawAndAnimateTriangle(angleObject, -2 * displacement, 2 * displacement, -1.0f, 1.0f, -2.0f, 2.0f, colorYellow);
 
 		// 4 outer pink
-		drawAndAnimateTriangle(3 * displacement, displacement, 2.0f, 0.0f, 3.0f, 1.0f, colorPink);
-		drawAndAnimateTriangle(displacement, -3 * displacement, 0.0f, -2.0f, 1.0f, -3.0f, colorPink);
-		drawAndAnimateTriangle(-3 * displacement, -displacement, -2.0f, 0.0f, -3.0f, -1.0f, colorPink);
-		drawAndAnimateTriangle(-displacement, 3 * displacement, 0.0f, 2.0f, -1.0f, 3.0f, colorPink);
+		drawAndAnimateTriangle(angleObject, 3 * displacement, displacement, 2.0f, 0.0f, 3.0f, 1.0f, colorPink);
+		drawAndAnimateTriangle(angleObject, displacement, -3 * displacement, 0.0f, -2.0f, 1.0f, -3.0f, colorPink);
+		drawAndAnimateTriangle(angleObject, -3 * displacement, -displacement, -2.0f, 0.0f, -3.0f, -1.0f, colorPink);
+		drawAndAnimateTriangle(angleObject, -displacement, 3 * displacement, 0.0f, 2.0f, -1.0f, 3.0f, colorPink);
 	glPopMatrix();
 
-	if (angle < 360)
-		angle += (float)ROTATION_UNIT;
+	
+	if (angleObject < 360.0f)
+	{
+		angleObject += (float)ROTATION_UNIT;
+		angleCenter -= (float)ROTATION_UNIT;
+	}
 	else
 	{
-		angle = 0.0f;
-		if (counter < 10)
-			displacement += 0.03f;
-		else if (counter < 20)
-			displacement -= 0.03f;
-		else
-			counter = 0;
-		counter++;
+		angleObject = 0.0f;
+		angleCenter = 360.0f;
 	}
+
+	/*
+	if (counter < 10)
+		displacement += 0.05;
+	else if (counter < 20)
+		displacement -= 0.05;
+	else
+		counter = 0;
+		
+	counter++;
+	*/
 
 	glutSwapBuffers();
 }
